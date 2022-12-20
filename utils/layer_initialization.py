9aafd398ac4c1046650cd78fa316e2e18cc7a689
christopher schicho <christopher.schicho@gmail.com>
2022-12-20 20:51:04 +0100

    #18-add ResNet benchmark model

--- utils/layer_initialization.py
+++ utils/layer_initialization.py
@@ -6,11 +6,14 @@ from torch.nn import Linear, Conv2d, Parameter
 
 def zero_initialize_layer(layer: Union[Linear, Conv2d], mode: str, factor: float):
     layer.weight = Parameter(torch.zeros_like(layer.weight, requires_grad=True))
-    if mode == "normal":
-        layer.bias = Parameter(torch.randn_like(layer.bias, requires_grad=True) * factor)
-    elif mode == "uniform":
-        layer.bias = Parameter(torch.rand_like(layer.bias, requires_grad=True) * factor)
-    elif mode == "zero":
-        layer.bias = Parameter(torch.zeros_like(layer.bias, requires_grad=True) * factor)
+    if layer.bias is not None:
+        if mode == "normal":
+            layer.bias = Parameter(torch.randn_like(layer.bias, requires_grad=True) * factor)
+        elif mode == "uniform":
+            layer.bias = Parameter(torch.rand_like(layer.bias, requires_grad=True) * factor)
+        elif mode == "zero":
+            layer.bias = Parameter(torch.zeros_like(layer.bias, requires_grad=True) * factor)
+        else:
+            raise ValueError(f"mode {mode} is not supported")
     else:
-        raise ValueError(f"mode {mode} is not supported")
+        print(f"Cannot initialize bias for layer {layer}")
